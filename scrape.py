dcb53814689f5f4c8910a90594dff7074543ea2b
nyxsys <isaprettycoolguy@gmail.com>
2016-03-02 17:08:00 -0500

    jacob fixed login script

--- scrape.py
+++ scrape.py
@@ -1,41 +1,27 @@
+import requests, time
+from datetime import datetime
 from bs4 import BeautifulSoup
-import requests, certifi, urllib3
-#nitethemooks
-#testtest
-
-user = "nitethemooks"
-
-
 def main():
-    global user
-    login_url = "https://twitter.com/login"
-    session_requests = requests.session()
-    getpage(session_requests, login_url)
-    result = session_requests.get(
-    "https://twitter.com/{0}/following".format(user),
-    headers = dict(referer=login_url))
-    soup = BeautifulSoup(result.text,"html.parser")
-    #followers = soup.find_all(attrs={"class":"ProfileCard-screennameLink"})
-    print(soup.title)
-    
-    
-def getpage(sr, url):
-    payload = {}
-    
-    result = sr.get(url)
-    soup = BeautifulSoup(result.text,'html.parser')
-    payload["session[username_or_email]"] = "nitethemooks"
-    payload["session[password]"] = "testtest"
-    payload["authenticity_token"] = str(soup.find(attrs={"name":"authenticity_token"})["value"])
-    payload["ui_metrics_seed"] = str(soup.find(attrs={"name":"ui_metrics_seed"})["value"])
-    print(payload)
-    result = sr.post(
-    url,
-    data=payload,
-    headers = dict(referer=url)
-    )
-    print(result.status_code)
-    soup = BeautifulSoup(result.text,"html.parser")
-    print(soup.title)
-    return sr
-main();
+    username = "nitethemooks"
+    password = "testtest"    
+    session = requests.Session()
+    response = session.get("https://twitter.com/login")
+    soup1 = BeautifulSoup(response.text, 'html.parser')
+    loginToken = soup1.find('input', {'name': 'authenticity_token'})
+    token = loginToken.get('value')
+    response = session.post("https://twitter.com/sessions", data={
+        'session[username_or_email]': username,
+        'session[password]': password,
+        'authenticity_token': token
+    })
+    soup2 = BeautifulSoup(response.text, 'html.parser')
+    print soup2.title
+    print soup2.find_all(class_="ProfileCardStats-statValue")
+    return 0
+    if __name__ == "__main__":
+        start_time = time.time()
+        exit_code = main()
+        if(exit_code == 0):
+            print "--- Completed in %d seconds ---" % (time.time() - start_time) 
+            
+main()
